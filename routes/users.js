8aaddbea8daff6932a8a949f15c1ab1882f1bbf8
rubenrooms <ruben_rooms@hotmail.com>
2021-08-20 19:17:56 +0200

    add: login function with passport + tested with postman

--- routes/users.js
+++ routes/users.js
@@ -8,5 +8,5 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/signup', authController.signup);
-//router.post('/login', authController.login);
+router.post('/login', authController.login);
 module.exports = router;
